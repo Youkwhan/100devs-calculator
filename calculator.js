0430696f09bb91d95b4050520f0da3d3cd0846df
Youkwhan <Youkwhan@gmail.com>
2024-01-19 20:01:59 -0500

    Add js

--- calculator.js
+++ calculator.js
@@ -1,21 +1,21 @@
+result = document.getElementById("result");
+
 function appendDigit(value) {
-  document.getElementById("result").value += value;
+  result.value += value;
 }
 
 function appendOperator(operator) {
-  document.getElementById("result").value += operator;
+  result.value += operator;
 }
 
 function clearResult() {
-  document.getElementById("result").value = "";
+  result.value = "";
 }
 
 function calculateResult() {
   try {
-    document.getElementById("result").value = eval(
-      document.getElementById("result").value
-    );
+    result.value = eval(result.value);
   } catch (error) {
-    document.getElementById("result").value = "Error";
+    result.value = "Error";
   }
 }
